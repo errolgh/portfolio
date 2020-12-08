789b040ce0ec79f0d4071e2efe71cb0f1d4b42f4
errolgh <ewatson@ggoutfitters.com>
2020-12-08 18:51:37 -0500

    created nav bar

---
+++ src/components/Nav.js
@@ -0,0 +1,14 @@
+import '../styles/nav.scss'
+
+export default function Nav() {
+    return (
+        <nav>
+            <ul>
+                <li>Errol Watson</li>
+                <li>Case Studies</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+    )
+}
