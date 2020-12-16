2c0a0768f975abc76103d98c4551d74b5f54b3be
errolgh <ewatson@ggoutfitters.com>
2020-12-15 20:55:18 -0500

    Adding projects

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,9 +1,34 @@
+import hackerNewsSearch from '../images/hackernewssearch.png'
+import '../styles/gallery.scss'
+
+
 export default function Gallery(){
     return (
         <div>
-            <div>
-                <h3>Hacker News Search</h3>
+            <div className="card-container">
+                <div className="project-card">
+                    <h3>Hacker News Search</h3>
+                    <img src={hackerNewsSearch} alt="Hacker News Search" />
+                    <p></p>
+                </div>
+                <div className="project-card">
+                    <h3>Hacker News Search</h3>
+                    <img src={hackerNewsSearch} alt="Hacker News Search" />
+                    <p></p>
+                </div>
+                <div className="project-card">
+                    <h3>Hacker News Search</h3>
+                    <img src={hackerNewsSearch} alt="Hacker News Search" />
+                    <p></p>
+                </div>
             </div>
         </div>
     )
 }
+
+// app title
+// app image or GIF
+// short description
+// github
+// make a visit link (button)
+// show more nerd shit
